070aa021944cf5f397cdae27194415bec9a5642d
Tejas151106 <gandlurisaitejasnvn@gmail.com>
2025-10-01 00:57:30 +0530

    Create Insertion at head.cpp

---
+++ Insertion at head.cpp
@@ -0,0 +1,59 @@
+/*
+EXPERIMENT 17
+Name :Gandluri Sai Tejas
+PRN  : 24070123045
+Class: ENTC A2
+*/
+
+#include <iostream>
+using namespace std;
+
+class Link {
+    public:
+    int data;
+    Link* next;
+
+    // Constructor
+    Link(int num) {
+        data = num;
+        next = NULL;
+    }
+};
+
+// Insert a new node at the head
+void insert_head(Link* &head, int data) {
+    Link* new_node = new Link(data);   
+    new_node->next = head;             
+    head = new_node;           
+}
+
+void disp(Link* head) {
+    Link* temp = head;  
+    while (temp != NULL) {
+        cout << temp->data << " --> ";
+        temp = temp->next;
+    }
+    cout << "NULL" << endl;
+}
+
+int main() {
+    Link* head = NULL;  
+ 
+    insert_head(head, 30);
+    disp(head);
+
+    insert_head(head, 32);
+    disp(head);
+
+    insert_head(head, 35);
+    disp(head);
+
+    return 0;
+}
+
+/*
+Output:
+30 --> NULL
+32 --> 30 --> NULL
+35 --> 32 --> 30 --> NULL
+*/
